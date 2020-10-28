9e71e6e12b57ade669cf205b2ba056631688dd46
marcelo ivan <sagacello@hotmail.com>
2020-10-28 13:14:22 -0300

    casa_cambio

---
+++ casa_cambio/script.js
@@ -0,0 +1,77 @@
+window.onload = () => {
+    setupEventHandlers();
+  }
+  
+  const setupEventHandlers = () => {
+    const searchButton = document.querySelector('#search-button');
+    searchButton.addEventListener('click', handleSearchEvent);
+  }
+  
+  const handleSearchEvent = () => {
+    const currency = document.querySelector('#currency-input').value;
+    const currencyUpperCased = currency.toUpperCase();
+  
+    cleanList();
+    
+    if (currency === '') {
+      showAlert('A moeda deve ser informada');
+    } else {
+      // fetchCurrency(currencyUpperCased);
+      fetchCurrencyAwaitAsync(currencyUpperCased);
+    }
+  }
+  
+  const showAlert = (message) => {
+    window.alert(message);
+  }
+  
+  // const fetchCurrency = (currency) => {
+  //   const endpoint = `https://api.ratesapi.io/api/latest?base=${currency}`;
+  
+  //   fetch(endpoint)
+  //     .then((response) => response.json())
+  //     .then((object) => {
+  //       if (object.error) {
+  //         throw new Error(object.error)
+  //       } else {
+  //         handleRates(object.rates);
+  //       }
+  //     })
+  //     .catch((error) => showAlert(error));
+  // }
+  
+  const fetchCurrencyAwaitAsync = async (currency) => {
+    const endpoint = `https://api.ratesapi.io/api/latest?base=${currency}`;
+  
+    try {
+      const response = await fetch(endpoint);
+      const object = await response.json();
+  
+      if (object.error) {
+        throw new Error(object.error);
+      } else {
+        handleRates(object.rates);
+      }
+    } catch (error) {
+      showAlert(error);
+    }
+  }
+  
+  const handleRates = (rates) => {
+    const ratesEntries = Object.entries(rates);
+  
+    // ratesEntries.forEach(renderRate);
+    ratesEntries.forEach((entry) => renderRate(entry));
+  }
+  
+  const renderRate = ([ currency, value ]) => {
+    const ul = document.querySelector('#currency-list');
+    const li = document.createElement("li");
+    li.innerHTML = `${currency}: ${value}`
+    ul.appendChild(li)
+  }
+  
+  const cleanList = () => {
+    const ul = document.querySelector('#currency-list');
+    ul.innerHTML = '';
+  }
